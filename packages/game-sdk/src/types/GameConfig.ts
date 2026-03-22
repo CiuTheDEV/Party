f652972934f6cc31adcadb2dd8ec08bcd8af8340
CiuTheDEV <ciu.ciubiczys@gmail.com>
2026-03-22 20:06:38 +0100

    feat: add GameConfig type to game-sdk

---
+++ packages/game-sdk/src/types/GameConfig.ts
@@ -0,0 +1,11 @@
+export type GameConfig = {
+  id: string
+  name: string
+  description: string
+  icon: string
+  minPlayers: number
+  maxPlayers: number
+  isPremium: boolean
+  color: string
+  href: string
+}
